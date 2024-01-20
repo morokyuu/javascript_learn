442c7600ce7e2b00fa62430e43328227a4c935fa
zota <zota@example.com>
2024-01-20 17:25:05 +0900

    exercise

--- lecture61/main.js
+++ lecture61/main.js
@@ -2,10 +2,16 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function(e){
-    let inputColorName = document.getElementById('inputColorName');
-    let colorNameText = inputColorName.value;
-    let inputName = inputColorName.getAttribute('name');
+    let inputA = document.getElementById('inputA');
+    let inputAtext = inputA.value;
+    let inputAName = inputA.getAttribute('name');
+
+    let inputB = document.getElementById('inputB');
+    let inputBtext = inputB.value;
+    let inputBName = inputB.getAttribute('name');
+
     let showText = document.getElementById('showText');
-    showText.textContent = `name属性: ${inputName}, 入力されたテキスト: ${colorNameText}`
+    showTextA.textContent = `name属性: ${inputAName}, 入力されたテキスト: ${inputAtext}`
+    showTextB.textContent = `name属性: ${inputBName}, 入力されたテキスト: ${inputBtext}`
 
 }, false);
